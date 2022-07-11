931b7acc2cd77e05fdf30a4cdc70d792a9a44cdb
eyoun-4319 <eyoun4319@icloud.com>
2022-07-11 11:11:44 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,37 +1,43 @@
 // JavaScript Document
 
+
 const api = {
-	key: "e0b35d5e45d6e820069747726618c80a",
-	baseurl: "https://api.openweathermap.org/data/2.5",
+  key: "e0b35d5e45d6e820069747726618c80a",
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
-	if (evt.keyCode == 13) {
-		getResults(searchbox.value);
-	}
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
 }
 
 function getResults (query) {
-	fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
-	.then(weather => {
-		return weather.json();
-	}).then(displayResults);
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
 }
 
 function displayResults (weather) {
-	console.log(weather);
-	let city = document.querySelector('.location .city');
-	city.innerText = `${weather.name}, ${weather.sys.country}`;
-	
-	let now = new Date();
-	let dae = document.querySelector('.location .date');
-	date.innerText = dateBuilder(now);
-	
-	let temp = document.querySelector('.current .temp');
-	temp.innerHTML = `${Math.round(weather.main.temp_min)}&deg;f / ${math.round(weather.main.temp_max)}&deg;f`;
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 }
 
 function dateBuilder (d) {
